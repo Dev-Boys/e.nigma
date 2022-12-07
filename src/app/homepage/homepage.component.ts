72176a210bffa8d03d0798969089cd3e131ae796
GabsGabsy <gabrielmendonca404@gmail.com>
2022-12-06 21:49:58 -0300

    build

---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css']
+})
+export class HomepageComponent {
+
+theme = "assets/img/theme-transp.png"; 
+cor = "#000000"
+play = "assets/img/play-icon-transp.png"
+
+
+ 
+}
